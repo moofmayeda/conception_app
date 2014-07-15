f8ad56dd0cfdcb70d09b810441abed5448a81db4
moof <moofmayeda@yahoo.com>
2014-07-14 18:37:50 -0700

    created html, wrote jquery for conception date function

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,20 @@ var conceptionDate = function(date, early, late) {
 	birthday.setDate(birthdayDate-pregnancyLength);
 	return (birthday.getMonth()+1) + "/" + birthday.getDate() + "/" + birthday.getFullYear();
 };
+
+$(document).ready(function() {
+	$("form").submit(function(event) {
+		event.preventDefault();
+	var date = $("input#birthday-month").val()+"/"+$("input#birthday-date").val()+"/"+$("input#birthday-year").val();
+	var early = $("input#early-birth").val();
+	var late = $("input#late-birth").val();
+	var result = conceptionDate(date, early, late);
+	$("#conception-date").text(result);
+	$("#results").show();
+	$("input#birthday-month").val("");
+	$("input#birthday-date").val("");
+	$("input#birthday-year").val("");
+	$("input#early-birth").val("");
+	$("input#late-birth").val("");
+	});
+});
